6d02cf5181b7fffd4d0f89454e8dc3b5c87326c7
Ignacy Moryc <imoryc@gmail.com>
2016-08-08 12:54:13 +0200

    Switch to template for presenting string output

--- presenters/string_presenter.go
+++ presenters/string_presenter.go
@@ -1,21 +1,37 @@
 package presenters
 
 import (
-	"github.com/google/go-github/github"
-	"log"
-	"strings"
+    "html/template"
+    "log"
+    "os"
+    "strings"
+
+    "github.com/google/go-github/github"
 )
 
+const templ = `{{ .Number | printf "%d" }} | {{ .Title }}
+Labels: {{ .AllLabels }}
+`
+
+var report = template.Must(template.New("issueList").Parse(templ))
+
+type Result struct {
+    Number           int
+    Title, AllLabels string
+}
+
 func PrintPullRequestData(pullRequests []*github.Issue) {
-	for _, pr := range pullRequests {
-		title := *pr.Title
-		number := *pr.Number
-		labels := []string{}
-		for _, l := range pr.Labels {
-			labels = append(labels, *l.Name)
-		}
-		all_labels := strings.Join(labels, ",")
-		log.Printf("%4d | %s", number, title)
-		log.Printf("Labels: %s", all_labels)
-	}
+    for _, pr := range pullRequests {
+        title := *pr.Title
+        number := *pr.Number
+        labels := []string{}
+        for _, l := range pr.Labels {
+            labels = append(labels, *l.Name)
+        }
+        allLabels := strings.Join(labels, ",")
+        result := &Result{number, title, allLabels}
+        if err := report.Execute(os.Stdout, result); err != nil {
+            log.Fatal(err)
+        }
+    }
 }
